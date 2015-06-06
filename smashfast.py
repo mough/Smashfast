cb9f63db41f8991440a1f431874999bf3ab4f109
Laurie Morse <lmorse@sbcglobal.net>
2015-06-06 10:11:02 -0500

    adding smashfast.py 2

--- smashfast.py
+++ smashfast.py
@@ -1,61 +1,7 @@
 __author__ = 'laurie'
 
-
-class Scene(object):
-
-    def enter(self):
-        pass
-
-
-class Engine(object):
-
-    def __init__(self, scene_map):
-        pass
-
-    def play(self):
-        pass
-
-class Death(Scene):
-
-    def enter(self):
-        pass
-
-class CentralCorridor(Scene):
-
-    def enter(self):
-        pass
-
-class LaserWeaponArmory(Scene):
-
-    def enter(self):
-        pass
-
-class TheBridge(Scene):
-
-    def enter(self):
-        pass
-
-class EscapePod(Scene):
-
-    def enter(self):
-        pass
-
-
-class Map(object):
-
-    def __init__(self, start_scene):
-        pass
-
-    def next_scene(self, scene_name):
-        pass
-
-    def opening_scene(self):
-        pass
-
-
-a_map = Map('central_corridor')
-a_game = Engine(a_map)
-a_game.play()
+from sys import exit
+from random import randint
 
 class Scene(object):
 
@@ -265,3 +211,8 @@ class Map(object):
 
     def opening_scene(self):
         return self.next_scene(self.start_scene)
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
